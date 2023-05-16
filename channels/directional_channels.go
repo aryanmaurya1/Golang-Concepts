e97221b7316154c76b009457bec91a6b05434ae4
Aryan Maurya <aryanmaurya1@outlook.com>
2023-05-17 00:01:52 +0530

    Commented errors which were preventing directional_channels.go from compiling.

--- channels/directional_channels.go
+++ channels/directional_channels.go
@@ -4,19 +4,28 @@ import (
 	"fmt"
 )
 
-func sendToReceiveOnlyChannel(c <-chan int) {
-	c <- 100
-}
+// There are three types on channels direction wise
+// 1. receive-only channels (<-chan T)
+// 2. send-only channels  (chan<- T)
+// 3. bidirectional channels (chan T)
+
+// func sendToReceiveOnlyChannel(c <-chan int) {
+// 	c <- 100
+// }
 
 // OUTPUT
 // channels/directional_channels.go:8:2: invalid operation: cannot send to receive-only channel c (variable of type <-chan int)
 
-func receiveFromSendOnlyChannel(c chan<- int) {
-	fmt.Println(<-c)
-}
+// -----------------------------------------------------
+
+// func receiveFromSendOnlyChannel(c chan<- int) {
+// 	fmt.Println(<-c)
+// }
 
 // OUTPUT
 // channels/directional_channels.go:15:16: invalid operation: cannot receive from send-only channel c (variable of type chan<- int)
+
+// -----------------------------------------------------
 
 
 func sendAndReceiveBothFromChannel(c chan int){
