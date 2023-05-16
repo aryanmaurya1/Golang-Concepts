ff5f24debae9961f0217e08c8ab17c6b2291b87d
Aryan Maurya <aryanmaurya1@outlook.com>
2023-05-16 23:55:58 +0530

    Added directional_channels.go file.

---
+++ channels/directional_channels.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sendToReceiveOnlyChannel(c <-chan int) {
+	c <- 100
+}
+
+// OUTPUT
+// channels/directional_channels.go:8:2: invalid operation: cannot send to receive-only channel c (variable of type <-chan int)
+
+func receiveFromSendOnlyChannel(c chan<- int) {
+	fmt.Println(<-c)
+}
+
+// OUTPUT
+// channels/directional_channels.go:15:16: invalid operation: cannot receive from send-only channel c (variable of type chan<- int)
+
+
+func sendAndReceiveBothFromChannel(c chan int){
+	c <- 10
+	fmt.Println(<-c)
+}
+// OUTPUT
+// 10
+
+
+func DirectionalChannels() {
+	var c chan int = make(chan int, 1)
+	sendAndReceiveBothFromChannel(c)
+}
